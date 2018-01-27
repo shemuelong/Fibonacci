1af847f793179d8f703ad8b3e075c278bdbc383d
shemuelong <31332054+shemuelong@users.noreply.github.com>
2018-01-27 12:03:32 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <ctime>
+
+unsigned long long fibonacciAddition(int n)
+{
+    unsigned long long a = 0, b = 1, c, i;
+    if (n == 0)
+        return a;
+    for (i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+unsigned long long fibonacci(int a){
+    if (a == 0 || a == 1)
+        return 1;
+    else
+        return fibonacci(a-1) + fibonacci(a-2);
+}
+
+void dynamic_fib(int a){
+    auto* fib = new unsigned long long[a];
+    fib[0] = 1;
+    if(a > 0)fib[1] = 1;
+    if (a == 0){
+        std::cout << 0 << std::endl;
+        delete[] fib;
+        return;
+    }
+    for (int i = 2; i < a; i++){
+        fib[i] = fib[i-1]+fib[i-2];
+        //std::cout << "fib" << i << ": " << fib[i] << std::endl;
+    }
+    std::cout << fib[a-1] << std::endl;
+
+    delete[] fib;
+}
+
+int main() {
+    clock_t t1, t2;
+
+   for(int i = 0; i < 30; i ++){
+        std::cout << "i: " << i <<" fibonacciAddition(i): " << fibonacciAddition(i) << std::endl;
+    }
+    std::cout << std::endl;
+
+    for(int i = 0; i < 30; i ++){
+        std::cout << "i: " << i <<" dynamic_fib(i): ";
+        dynamic_fib(i);
+    }
+    std::cout << std::endl;
+
+    for(int i = 0; i < 40;i++){
+        t1 = clock();
+        std::cout << "i: " << i <<" fibonacci(i): " << fibonacci(i) << " time: ";
+        t2 = clock();
+        float diff = (static_cast<float>(t2) - (float)t1);
+        std::cout << diff << std::endl;
+    }
+    
+    return 0;
+}
